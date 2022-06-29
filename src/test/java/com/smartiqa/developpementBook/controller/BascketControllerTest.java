96ed7ab3321ba6d7afbd4b202132a6e280015c99
chafik.gouasmia@axa.dz <chafik.gouasmia@axa.dz>
2022-06-29 19:13:24 +0100

    Added failling test for book model cration (red)

--- src/test/java/com/smartiqa/developpementBook/controller/BascketControllerTest.java
+++ src/test/java/com/smartiqa/developpementBook/controller/BascketControllerTest.java
@@ -1,25 +1,44 @@
 package com.smartiqa.developpementBook.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @AutoConfigureMockMvc
 @SpringBootTest
 public class BascketControllerTest {
 
     //Inject Support Utils
-    private MockHttpServletRequest request;
+    private static MockHttpServletRequest request;
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+    public static final MediaType APPLICATION_JSON_UTF8 = MediaType.APPLICATION_JSON;
 
     // Inject services
     @Mock
-    BascketService bascketService ;
+    BascketService bascketService;
 
+    @BeforeAll
+    public static void setup() {
+
+        request = new MockHttpServletRequest();
+
+    }
     @Test
-    public  void  placeHolderTest(){
-
+    public Double calculateDiscountBascketTest (){
+        List<Book> bookList = new ArrayList<Book>();
     }
 
 }
