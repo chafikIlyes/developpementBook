b5dc5ae99498abcbb1bf12ea46fc0a9db362ee8f
chafik.gouasmia@axa.dz <chafik.gouasmia@axa.dz>
2022-06-29 14:44:22 +0100

    initial commit

---
+++ src/main/java/com/smartiqa/developpementBook/DeveloppementBookApplication.java
@@ -0,0 +1,13 @@
+package com.smartiqa.developpementBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloppementBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloppementBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartiqa/developpementBook/DeveloppementBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartiqa.developpementBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloppementBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
